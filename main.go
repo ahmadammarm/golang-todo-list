5779d88e17d4f3ae113b1103406b84af5435a800
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-11-01 11:24:32 +0700

    feat: add supabase connection

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+func DatabaseInit() (*sql.DB, error) {
+	_ = godotenv.Load()
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
+		user, password, host, port, name, sslmode)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
 
 func main() {
-    fmt.Println("Hello todo list")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Warning: .env file not found")
+	}
+
+	db, err := DatabaseInit()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	app := fiber.New()
+	app.Listen(":8000")
 }
